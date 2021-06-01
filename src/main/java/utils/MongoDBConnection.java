f1d975daf8399e49e558bf8b88e25e4277b33d14
giusya <g.annunziata49@studenti.unisa.it>
2021-06-01 17:12:55 +0200

    Modifica al database

--- src/main/java/utils/MongoDBConnection.java
+++ src/main/java/utils/MongoDBConnection.java
@@ -7,7 +7,7 @@ public class MongoDBConnection {
 
     public static synchronized MongoDatabase getDatabase() {
         MongoClient mongo = new MongoClient("localhost", 27017);
-        MongoDatabase database = mongo.getDatabase("movies");
+        MongoDatabase database = mongo.getDatabase("movieverse");
         return database;
     }
 }
